90b96d84912f2d6f7806f05331d4b97d96221163
jhkratoz <117241769+jhkratoz@users.noreply.github.com>
2022-11-29 20:49:23 -0500

    adding df

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ def main():
         st.pyplot(fig3)
 
         st.write('Average Accuracy: ', round(sum(acc)/len(acc),2), '%')
+        st.write('Table Output:')
         st.write(coef_df)
 
     with survey_doc:
